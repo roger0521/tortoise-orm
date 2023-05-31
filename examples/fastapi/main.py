cfcc6d26fe43464669bd1382873db29271ab7e3d
Roger Tsai <reliability0521@gmail.com>
2023-05-31 16:09:48 +0800

    refactoring

--- examples/fastapi/main.py
+++ examples/fastapi/main.py
@@ -1,10 +1,6 @@
 # pylint: disable=E0611,E0401
-import imp
-from typing import List
 from configs.config import TORTOISE_ORM
-from fastapi import FastAPI, HTTPException
-from models.db_models import User_Pydantic, UserIn_Pydantic, Users, Event
-from pydantic import BaseModel
+from fastapi import FastAPI
 from routers.user_router import router as UserRouter
 from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 
